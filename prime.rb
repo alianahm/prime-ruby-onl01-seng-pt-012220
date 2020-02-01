5c2efc2fc59254284a05c34828e83740cd1da0e1
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-01 06:39:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,13 @@
 # Add  code here!
 
+def prime?(number)
+  if number < 2
+     return false
+  else 
+    (2..number - 1).to_a.all? do |n|
+      number % n != 0
+    end
+  end 
+end
+
+prime?(-1)
